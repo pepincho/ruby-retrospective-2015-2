f38fc14f5450f20364974214765196b2cf65988a
Petar Ivanov <pepincho95@gmail.com>
2016-01-27 23:26:42 +0200

    Add some improved task's solutions.

--- solutions/05.rb
+++ solutions/05.rb
@@ -14,115 +14,118 @@ class ObjectStore
   end
 
   def add(name, object)
-    @current_branch = @branch_manager.get_current_branch
+    @branch = @branch_manager.branch
 
     new_file = File.new(name, object)
     update_file(new_file)
-    @current_branch.changes += 1
+    @branch.changes += 1
 
     OperationResult.new("Added #{new_file.name} to stage.", true, object)
   end
 
   def commit(message)
-    if @branch_manager.get_current_branch.changes == 0
+    if @branch_manager.branch.changes == 0
       OperationResult.new("Nothing to commit, working directory clean.", false)
     else
-      new_commit = Commit.new(@current_branch.local_all_files,
+      new_commit = Commit.new(@branch.local_all_files,
         message, Time.now)
-      @current_branch.commits << new_commit
-      @current_branch.global_all_files = @current_branch.local_all_files.clone
-      changes = @current_branch.changes
-      @current_branch.changes = 0
+      @branch.commits << new_commit
+      @branch.global_all_files = @branch.local_all_files.clone
+      changes = @branch.changes
+      @branch.changes = 0
       OperationResult.new("#{new_commit.message}\n\t#{changes}" +
         " objects changed", true, new_commit)
     end
   end
 
   def update_file(new_file)
-    @current_branch = @branch_manager.get_current_branch
+    @branch = @branch_manager.branch
 
-    existing_file = @current_branch.local_all_files.select { |f|
+    existing_file = @branch.local_all_files.select { |f|
       f.name == new_file.name }
     if existing_file != []
-      @current_branch.local_all_files -= existing_file
-      @current_branch.local_all_files << new_file
+      @branch.local_all_files -= existing_file
+      @branch.local_all_files << new_file
     else
-      @current_branch.local_all_files << new_file
+      @branch.local_all_files << new_file
     end
   end
 
-  def remove(name)
-    @current_branch = @branch_manager.get_current_branch
+  REMOVE = "Added %{name} for removal."
 
-    old_file = @current_branch.local_all_files.select { |f| f.name == name }
+  def remove(name)
+    @branch = @branch_manager.branch
+    old_file = @branch.local_all_files.select { |f| f.name == name }
     if old_file != []
-      @current_branch.local_all_files -= old_file
-      @current_branch.changes += 1
-      OperationResult.new("Added #{name} for removal.", true, old_file.
-        first.obj)
+      @branch.local_all_files -= old_file
+      @branch.changes += 1
+      OperationResult.new(REMOVE % { name: name }, true, old_file.first.obj)
     else
       OperationResult.new("Object #{name} is not committed.", false)
     end
   end
 
-  def checkout(commit_hash)
-    @current_branch = @branch_manager.get_current_branch
+  CHECKOUT = "HEAD is now at %{hash}."
 
-    new_head = @current_branch.commits.select { |c| c.hash == commit_hash }
-    if new_head != []
-      @current_branch.commits = @current_branch.commits[
-        0..@current_branch.commits.index(new_head.first)]
-      update_branch(new_head.first)
-      OperationResult.new("HEAD is now at #{commit_hash}.", true,
-        new_head.first)
+  def checkout(hash)
+    @branch = @branch_manager.branch
+    head = @branch.commits.select { |c| c.hash == hash }
+    if head != []
+      @branch.commits = @branch.commits[0..@branch.commits.index(head.first)]
+      update_branch(head.first)
+      OperationResult.new(CHECKOUT % { hash: hash }, true, head.first)
     else
-      OperationResult.new("Commit #{commit_hash} does not exist.", false)
+      OperationResult.new("Commit #{hash} does not exist.", false)
     end
   end
 
   def update_branch(new_commit)
-    @current_branch = @branch_manager.get_current_branch
+    @branch = @branch_manager.branch
 
-    @current_branch.global_all_files = new_commit.objects.clone
-    @current_branch.local_all_files  = new_commit.objects.clone
+    @branch.global_all_files = new_commit.objects.clone
+    @branch.local_all_files  = new_commit.objects.clone
   end
 
   def branch
     @branch_manager
   end
 
+  LOG = "Branch %{name} does not have any commits yet."
+
+  ONE = "Commit #{hash}\nDate: "
+
+
   def log
-    if @branch_manager.get_current_branch.commits.size == 0
-      OperationResult.new("Branch #{@branch_manager.get_current_branch
-        .name} does" + " not have any commits yet.", false)
+    if @branch_manager.branch.commits.size == 0
+      OperationResult.new(LOG % { name: @branch_manager.branch.name
+        }, false)
     else
       message = []
-      @branch_manager.get_current_branch.commits.reverse.each do |commit|
+      @branch_manager.branch.commits.reverse.each do |commit|
         message << "Commit #{commit.hash}\nDate: " +
         "#{commit.date.strftime("%a %b %d %H:%M %Y %z")}" +
         "\n\n\t#{commit.message}"
       end
-      message = message.join("\n\n")
-      OperationResult.new(message, true)
+      OperationResult.new(message.join("\n\n"), true)
     end
   end
 
   def head
-    @current_branch = @branch_manager.get_current_branch
+    @branch = @branch_manager.branch
 
-    if @current_branch.commits.size == 0
-      OperationResult.new("Branch #{@current_branch.name} does" +
+    if @branch.commits.size == 0
+      OperationResult.new("Branch #{@branch.name} does" +
         " not have any commits yet.", false)
     else
-      OperationResult.new("#{@current_branch.commits[-1].message}", true,
-        @current_branch.commits[-1])
+      OperationResult.new("#{@branch.commits[-1].message}", true,
+        @branch.commits[-1])
     end
   end
 
   def get(name)
-    @current_branch = @branch_manager.get_current_branch
+    @branch = @branch_manager.branch
 
-    file = @current_branch.global_all_files.select { |f| f.name == name }
+    file = @branch.global_all_files.select { |f| f.name == name }
     if file != []
       OperationResult.new("Found object #{name}.", true, file.first.obj)
     else
@@ -151,31 +154,35 @@ class ObjectStore
       @branches = []
       new_branch = Branch.new("master")
       @branches << new_branch
-      @current_branch = new_branch
+      @branch = new_branch
     end
 
-    def get_current_branch
-      @current_branch
+    def branch
+      @branch
     end
 
     def create(branch_name)
       existing_branch = @branches.select { |b| b.name == branch_name }
       if existing_branch == []
         new_branch = Branch.new(branch_name)
-        new_branch.commits = @current_branch.commits.clone
-        new_branch.global_all_files = @current_branch.global_all_files.clone
-        new_branch.local_all_files = @current_branch.local_all_files.clone
-        @branches << new_branch
+        update_branch(new_branch)
         OperationResult.new("Created branch #{branch_name}.", true)
       else
         OperationResult.new("Branch #{branch_name} already exists.", false)
       end
     end
 
+    def update_branch(new_branch)
+      new_branch.commits = @branch.commits.clone
+      new_branch.global_all_files = @branch.global_all_files.clone
+      new_branch.local_all_files = @branch.local_all_files.clone
+      @branches << new_branch
+    end
+
     def checkout(branch_name)
       existing_branch = @branches.select { |b| b.name == branch_name }
       if existing_branch != []
-        @current_branch = existing_branch.first
+        @branch = existing_branch.first
         OperationResult.new("Switched to branch #{branch_name}.", true)
       else
         OperationResult.new("Branch #{branch_name} does not exist.", false)
@@ -185,7 +192,7 @@ class ObjectStore
     def remove(branch_name)
       old_branch = @branches.select { |b| b.name == branch_name }
 
-      if @current_branch.name == branch_name
+      if @branch.name == branch_name
         OperationResult.new("Cannot remove current branch.", false)
       elsif old_branch != []
         @branches -= old_branch
@@ -198,7 +205,7 @@ class ObjectStore
     def list
       message = []
       @branches.sort_by { |b| b.name }.each do |branch|
-        if branch == @current_branch
+        if branch == @branch
           message << "* #{branch.name}"
         else
           message << "  #{branch.name}"
@@ -214,7 +221,7 @@ class ObjectStore
     attr_accessor :objects
 
     def initialize(files, message, date)
-      @objects = files.clone
+      @objects = files.map(&:to_s).clone
       @message = message
       @date = date
       @hash = Digest::SHA1.hexdigest(
@@ -242,7 +249,7 @@ class ObjectStore
 
   class File < Struct.new(:name, :obj)
     def to_s
-    "#{name.to_s} - #{obj.to_s}"
+    "#{obj.to_s}"
     end
   end
 end
